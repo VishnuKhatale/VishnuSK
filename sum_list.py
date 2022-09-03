365bd133d94e2e37ed8ccc1d6dab6a5ccbd92849
Vishnu_Khatale <vishnu.khatale@innovationshub.in>
2022-09-03 11:44:25 +0530

    new branch

---
+++ sum_list.py
@@ -0,0 +1,17 @@
+l1=[3,5,76,8,4,3,5];
+
+# i=0;
+# sum1 = 0;
+# for i in l1:
+# 	sum1=sum1+i;
+# print(sum1);
+
+def sumlist(demo):
+	i=0;
+	sum1 = 0;
+	for i in demo:
+		sum1=sum1+i;
+	return sum1;
+
+print("vishnu khatale");
+print(sumlist(l1));
